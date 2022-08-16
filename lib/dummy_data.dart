35f2ab3dc6323f5f44429cc6e37c9d6564438826
FelipeOliveira <Felipe.Oliveira01@edu.pucrs.br>
2022-08-16 20:25:39 -0300

    code commit

---
+++ lib/dummy_data.dart
@@ -0,0 +1,65 @@
+import './models/customer.dart';
+
+const DUMMY_CUSTOMER = const [
+  Customer('1', 'Customername one', 0, false),
+  Customer('1', 'Customername two', 2, true),
+  Customer('1', 'Customername three', 1, true),
+  Customer('1', 'Customername four', 3, false),
+  Customer('1', 'Customername five', 1, false),
+  Customer('1', 'Customername six', 0, true),
+  Customer('1', 'Customername seven', 1, false),
+  Customer('1', 'Customername eight', 0, false),
+  Customer('1', 'Customername nine', 0, false),
+  Customer('1', 'Customername ten', 1, false),
+  Customer('1', 'Customername eleven', 0, false),
+  Customer('1', 'Customername twelve', 1, true),
+  Customer('1', 'Customername thirteen', 0, false),
+  Customer('1', 'Customername fourteen', 0, false),
+  Customer('1', 'Customername fiveteen', 1, false),
+  Customer('1', 'Customername sixteen', 0, false),
+  Customer('1', 'Customername zeventeen', 1, false),
+  Customer('1', 'Customername eighteen', 0, false),
+  Customer('1', 'Customername nineteen', 0, false),
+  Customer('1', 'Customername twenty', 0, false),
+];
+
+// const DUMMY_SALE = const [
+//   Sale(
+//     content: '€ 56.400,-',
+//     description: 'Value of sold packages',
+//     color: CustomColors.Red,
+//     type: Type.Customers
+//   ),
+//   Sale(
+//     content: '€ 12.854,-',
+//     description: 'Value in offerfase',
+//     color: CustomColors.Red,
+//     type: Type.Customers
+//   ),
+//   Sale(
+//     content: '156',
+//     description: 'Packages composed',
+//     color: CustomColors.Blue,
+//     type: Type.Packages
+//   ),
+//   Sale(
+//     content: '85',
+//     description: 'Packages to compose',
+//     color: CustomColors.Blue,
+//     type: Type.Packages
+//   ),
+//   Sale(
+//     content: 'Chocolate',
+//     description: 'Most used',
+//     color: CustomColors.Yellow,
+//     type: Type.Articles
+//   ),
+// ];
+
+// const DUMMY_PACKAGEDATE = const [
+//   PackageDate(date: 'Tuesday 22 November'),
+//   PackageDate(date: 'Wednesday 30 November'),
+//   PackageDate(date: 'Thursday 14 December'),
+// ];
+
+
---
+++ lib/models/customer.dart
@@ -0,0 +1,13 @@
+class Customer {
+  final String id;
+  final String name;
+  final int packagesCount;
+  final bool isFavorite;
+
+  const Customer(
+    this.id,
+    this.name,
+    this.packagesCount,
+    this.isFavorite,
+  );
+}
