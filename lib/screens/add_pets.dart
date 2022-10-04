567c89005b61ed6c0084cea583af800aa27910f1
Miguel_Castro <miguel_castro@dellteam.com>
2022-10-04 19:38:51 -0300

    create add screen

---
+++ lib/screens/add_pets.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AddPets extends StatefulWidget {
+  const AddPets({super.key});
+
+  @override
+  State<AddPets> createState() => _AddPetsState();
+}
+
+class _AddPetsState extends State<AddPets> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
