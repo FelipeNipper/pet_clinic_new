3b43e84515b76eea803b72134e51b62759326fc9
miguel-de-castro <miguel.c@edu.pucrs.br>
2022-11-01 03:33:31 -0300

    ajuste na rota get

--- lib/core/http_service.dart
+++ lib/core/http_service.dart
@@ -5,15 +5,11 @@ import 'package:http/http.dart' as http;
 import 'api_constants.dart';
 import 'http_error.dart';
 
-
 class HttpService {
-  final String controllerURL;
   final http.Client client = http.Client();
 
-  HttpService(this.controllerURL);
-
   Future<http.Response> get(String url) async {
-    String uri = ApiConstants.baseUrl + controllerURL + url;
+    String uri = ApiConstants.baseUrl + url;
     final http.Response response = await client.get(
       Uri.parse(uri),
     );
