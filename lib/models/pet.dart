59048df8ef4f10bc231323e01479574dcce166e9
miguel-de-castro <miguel.c@edu.pucrs.br>
2022-11-01 03:33:50 -0300

    ajustes de atributos

--- lib/models/pet.dart
+++ lib/models/pet.dart
@@ -1,38 +1,55 @@
+import '../core/util.dart';
+
 class Pet {
   final int id;
   final String nome;
-  final int idade;
-  final String raca;
-  final String image;
+  final String dataNascimento;
+  final String? image;
+  final Type tipo;
 
   const Pet(
       {required this.id,
       required this.nome,
-      required this.idade,
-      required this.raca,
-      required this.image});
+      required this.dataNascimento,
+      required this.tipo,
+      this.image});
 
   factory Pet.fromJson(Map<String, dynamic> json) {
     return Pet(
         id: json['id'] as int? ?? -1,
-        nome: json['nome'] as String? ?? '',
-        idade: json['idade'] as int? ?? -1,
-        raca: json['raca'] as String? ?? '',
-        image: json['image'] as String? ?? '');
+        nome: json['name'] as String? ?? '',
+        dataNascimento: json['birthDate'] as String? ?? '',
+        tipo: Type.fromJson(json['type']),
+        image: Util.getPhoto(Type.fromJson(json['type']).nome));
+  }
+}
+
+class Type {
+  final int? id;
+  final String nome;
+
+  const Type({this.id, required this.nome});
+
+  factory Type.fromJson(Map<String, dynamic> json) {
+    return Type(id: json['id'] as int? ?? 1, nome: json['name'] as String);
   }
 }
 
 class CreatePet {
   final String nome;
-  final int idade;
-  final String raca;
+  final String dataNascimento;
+  final String tipo;
 
   const CreatePet({
     required this.nome,
-    required this.idade,
-    required this.raca,
+    required this.dataNascimento,
+    required this.tipo,
   });
 
-  Pet toPet(int id) =>
-      Pet(id: id, nome: nome, idade: idade, raca: raca, image: 'no-pet.png');
+  Pet toPet(int id) => Pet(
+      id: id,
+      nome: nome,
+      dataNascimento: dataNascimento,
+      image: Util.getPhoto(tipo),
+      tipo: Type(id: 0, nome: tipo));
 }
